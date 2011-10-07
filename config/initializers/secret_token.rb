1c9ad55d2fee9ae05de45ced7ceb5081f6a3c247
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-10-06 22:44:33 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackedRailsApps::Application.config.secret_token = 'bd0d5ffaaa9ccc4d133272086f68fbb7ab2ae25a6ae4804ea6994b97b0e53dd44ac5f5188516f69c50f1ca7e17d7dc8a9ffb872694eaf10bf0951206615e728c'
